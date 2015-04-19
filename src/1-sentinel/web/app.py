f6809d3046e06410af2ad7cfd21b62736d5badd5
Tristan Carel <tristan.carel@gmail.com>
2015-04-19 17:26:09 +0200

    Redis: several nodes without --net=host

--- src/1-sentinel/web/app.py
+++ src/1-sentinel/web/app.py
@@ -2,8 +2,23 @@ from flask import Flask
 from redis.sentinel import Sentinel
 import os
 app = Flask(__name__)
-sentinel = Sentinel([('localhost', 26379)])
-redis = sentinel.master_for('meetup')
+
+sentinels = []
+i = 1
+# get sentinels (ip, port) from environment
+while True:
+	sentinel_ip_key = 'REDISSENTINEL%i_1_PORT_26379_TCP_ADDR' % i
+	if sentinel_ip_key in os.environ:
+		sentinels.append((
+			os.environ[sentinel_ip_key],
+			int(os.environ['REDISSENTINEL%i_PORT_26379_TCP_PORT' % i])
+		))
+		i += 1
+	else:
+		break		
+redis_cluster = os.environ.get('REDISSENTINEL1_ENV_CLUSTER_NAME')
+sentinel = Sentinel(sentinels)
+redis = sentinel.master_for(redis_cluster)
 
 @app.route('/')
 def hello():
